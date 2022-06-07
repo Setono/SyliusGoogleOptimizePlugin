a72b2b8e6d042e98e6c69785dc30b25ef6bd0330
Joachim Løvgaard <joachim@loevgaard.dk>
2022-06-07 09:03:28 +0200

    Add description

--- src/Context/BotRequestAwareVariantContext.php
+++ src/Context/BotRequestAwareVariantContext.php
@@ -8,6 +8,11 @@ use Setono\BotDetectionBundle\BotDetector\BotDetectorInterface;
 use Setono\SyliusGoogleOptimizePlugin\Factory\VariantFactoryInterface;
 use Setono\SyliusGoogleOptimizePlugin\Model\VariantInterface;
 
+/**
+ * The job of this variant context is to check whether the requesting user is a bot and if so always return the original.
+ * This is a decision made in the plugin so that we are certain that bots never see any variants and hence could disturb
+ * i.e. SEO reports etc.
+ */
 final class BotRequestAwareVariantContext implements VariantContextInterface
 {
     private VariantContextInterface $decoratedVariantContext;
@@ -16,8 +21,11 @@ final class BotRequestAwareVariantContext implements VariantContextInterface
 
     private VariantFactoryInterface $variantFactory;
 
-    public function __construct(VariantContextInterface $decoratedVariantContext, BotDetectorInterface $botDetector, VariantFactoryInterface $variantFactory)
-    {
+    public function __construct(
+        VariantContextInterface $decoratedVariantContext,
+        BotDetectorInterface $botDetector,
+        VariantFactoryInterface $variantFactory
+    ) {
         $this->decoratedVariantContext = $decoratedVariantContext;
         $this->botDetector = $botDetector;
         $this->variantFactory = $variantFactory;
